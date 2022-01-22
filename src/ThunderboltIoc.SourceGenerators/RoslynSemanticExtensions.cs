eebc6e48f864989c6c0d08d95d2462aa8bb22458
Aly Elhaddad <alyelhaddad96@gmail.com>
2022-01-22 21:28:50 +0200

    Removed unused extension methods in RoslynSemanticExtensions.

--- src/ThunderboltIoc.SourceGenerators/RoslynSemanticExtensions.cs
+++ src/ThunderboltIoc.SourceGenerators/RoslynSemanticExtensions.cs
@@ -7,15 +7,6 @@ namespace ThunderboltIoc.SourceGenerators;
 
 internal static class RoslynSemanticExtensions
 {
-    private static TResult MethodCache<T, TResult>(T input, Func<T, TResult> method, IDictionary<T, TResult> cache)
-    {
-        if (cache.ContainsKey(input))
-            return cache[input];
-        TResult result = method(input);
-        cache.Add(input, result);
-        return result;
-    }
-
     public static string GetFullNamespaceName(this INamespaceSymbol namespaceSymbol)
     {
         StringBuilder nsBuilder = new(namespaceSymbol.Name);
@@ -27,71 +18,30 @@ internal static class RoslynSemanticExtensions
         return nsBuilder.ToString();
     }
 
-    private static readonly Dictionary<ITypeSymbol, string> typeFullNamesCache = new(SymbolEqualityComparer.Default);
     public static string GetFullyQualifiedName(this ITypeSymbol typeSymbol)
     {
-        static string inv(ITypeSymbol typeSymbol) => $"{typeSymbol.ContainingNamespace.GetFullNamespaceName()}.{typeSymbol.Name}";
-        return MethodCache(typeSymbol, inv, typeFullNamesCache);
+        return $"{typeSymbol.ContainingNamespace.GetFullNamespaceName()}.{typeSymbol.Name}";
     }
 
-    private static readonly Dictionary<INamedTypeSymbol, string> namedTypeFullNamesCache = new(SymbolEqualityComparer.Default);
     public static string GetFullyQualifiedName(this INamedTypeSymbol namedTypeSymbol)
     {
-        static string inv(INamedTypeSymbol namedTypeSymbol) => $"{namedTypeSymbol.ContainingNamespace.GetFullNamespaceName()}.{namedTypeSymbol.Name}{(namedTypeSymbol.IsGenericType ? $"<{string.Join(", ", namedTypeSymbol.TypeArguments.Select(p => p.Name))}>" : "")}";
-        return MethodCache(namedTypeSymbol, inv, namedTypeFullNamesCache);
+        return $"{namedTypeSymbol.ContainingNamespace.GetFullNamespaceName()}.{namedTypeSymbol.Name}{(namedTypeSymbol.IsGenericType ? $"<{string.Join(", ", namedTypeSymbol.TypeArguments.Select(p => p.Name))}>" : "")}";
     }
 
-    private static readonly Dictionary<INamespaceSymbol, IEnumerable<INamedTypeSymbol>> getAllTypeMembersCache = new(SymbolEqualityComparer.Default);
     public static IEnumerable<INamedTypeSymbol> GetAllTypeMembers(this INamespaceSymbol namespaceSymbol)
     {
-        static IEnumerable<INamedTypeSymbol> inv(INamespaceSymbol namespaceSymbol)
+        foreach (INamedTypeSymbol child in namespaceSymbol.GetTypeMembers())
         {
-            foreach (INamedTypeSymbol child in namespaceSymbol.GetTypeMembers())
-            {
-                yield return child;
-                foreach (INamedTypeSymbol nestedChild in child.GetTypeMembers())
-                    yield return nestedChild;
-            }
-
-            foreach (INamedTypeSymbol childNamespaceTypeSymbol in namespaceSymbol.GetNamespaceMembers().SelectMany(ns => ns.GetAllTypeMembers()))
-                yield return childNamespaceTypeSymbol;
+            yield return child;
+            foreach (INamedTypeSymbol nestedChild in child.GetTypeMembers())
+                yield return nestedChild;
         }
-        return MethodCache(namespaceSymbol, inv, getAllTypeMembersCache);
+
+        foreach (INamedTypeSymbol childNamespaceTypeSymbol in namespaceSymbol.GetNamespaceMembers().SelectMany(ns => ns.GetAllTypeMembers()))
+            yield return childNamespaceTypeSymbol;
     }
 
     public static IEnumerable<INamedTypeSymbol> GetAllTypeMembers(this Compilation compilation) => compilation.GlobalNamespace.GetAllTypeMembers();
-    public static IEnumerable<INamespaceSymbol> GetAllNamespaceMembers(this INamespaceSymbol namespaceSymbol)
-    {
-        foreach (INamespaceSymbol parentNamespaceSymbol in namespaceSymbol.GetNamespaceMembers())
-        {
-            yield return parentNamespaceSymbol;
-            foreach (INamespaceSymbol childNamespaceSymbol in parentNamespaceSymbol.GetAllNamespaceMembers())
-                yield return childNamespaceSymbol;
-        }
-    }
-    public static IEnumerable<INamespaceSymbol> GetAllNamespaceMembers(this Compilation compilation)
-    {
-        return compilation.GlobalNamespace.GetAllNamespaceMembers();
-    }
-    public static IEnumerable<string> GetAllNamespaceNames(this Compilation compilation)
-    {
-        return compilation.GlobalNamespace.GetAllNamespaceMembers().Select(ns => ns.GetFullNamespaceName());
-    }
-
-    public static IEnumerable<string> GetAssemblyFullTypeNames(this Compilation compilation)
-    {
-        return compilation.GetAllTypeMembers().Select(t => t.GetFullyQualifiedName().RemovePrefix(Consts.global)).Where(t => compilation.Assembly.NamespaceNames.Any(ns => t.StartsWith(ns)) && compilation.Assembly.TypeNames.Any(tn => t.EndsWith(tn)));
-    }
-
-    public static IEnumerable<INamedTypeSymbol> GetAssemblyFullTypeMembers(this Compilation compilation)
-    {
-        return compilation.GetAllTypeMembers().Where(t =>
-        {
-            string typeName = t.GetFullyQualifiedName().RemovePrefix(Consts.global);
-            return compilation.Assembly.NamespaceNames.Any(ns => typeName.StartsWith(ns))
-                && compilation.Assembly.TypeNames.Any(tn => typeName.EndsWith(tn));
-        });
-    }
 
     public static bool HasParent(this ITypeSymbol typeSymbol, ITypeSymbol parentType)
     {
@@ -112,17 +62,11 @@ internal static class RoslynSemanticExtensions
         return semanticModel.GetSpeculativeTypeInfo(0, SyntaxFactory.ParseTypeName(fullName), SpeculativeBindingOption.BindAsTypeOrNamespace).Type as INamedTypeSymbol;
     }
 
-    public static IEnumerable<INamedTypeSymbol?> GetTypesByFullName(this Compilation compilation, params string[] fullNames)
-    {
-        SemanticModel semanticModel = compilation.GetSemanticModel(compilation.SyntaxTrees.First());
-        return
-            fullNames.Select(fullName => semanticModel.GetSpeculativeTypeInfo(0, SyntaxFactory.ParseTypeName(fullName), SpeculativeBindingOption.BindAsTypeOrNamespace).Type as INamedTypeSymbol);
-    }
-
     public static string GetMethodFullName(this IMethodSymbol method)
     {
         return $"{method.ContainingType.GetFullyQualifiedName()}.{method.Name}";
     }
+
     public static bool AreDefinitionsEqual(this IMethodSymbol method, IMethodSymbol equalMethod)
     {
         method = method.OriginalDefinition;
